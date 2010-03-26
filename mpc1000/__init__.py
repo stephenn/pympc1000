248881c6dfb46476a8a9c8657dbcfe2d9bcaaa85
Stephen Norum <stephen@mybunnyhug.org>
2010-03-26 02:18:46 -0700

    Cleaner doc strings and imports

--- mpc1000/__init__.py
+++ mpc1000/__init__.py
@@ -1,6 +1,6 @@
-'Load, edit, and export Akai MPC 1000 .pgm files.'
+'load, edit, and export Akai MPC 1000 program file data'
 
 # Re-import these for convenience
-from mpc1000.mpc1k import *
+from mpc1k import *
 
 __all__ = (mpc1k.__all__)
